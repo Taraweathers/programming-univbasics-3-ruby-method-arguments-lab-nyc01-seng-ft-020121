f97265c2483c0a56ada355034aa096f974b3d49f
Tara Weathers <tara.n.weathers@gmail.com>
2021-01-09 19:56:21 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,12 +1,11 @@
-def introduction(name)
-  puts "Hi, my name is #{name}."
+def a_method(a,b)
+  puts "I got #{a}"
+  puts "I got #{b}"
+  sum = a + b
+  puts "I got #{sum}"
 end
-def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-end
-def introduction_with_language_optional(name, language = "Ruby")
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-end
+ 
+a_method(2,3)
 
 
 
